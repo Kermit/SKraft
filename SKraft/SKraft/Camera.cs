d74865dd1c65d857510b5fdc767d828e34f6d76d
Arkadiusz Neuman <mrmoro@o2.pl>
2011-10-08 15:34:11 +0200

    First model and fpp camera
    Signed-off-by: Arkadiusz Neuman <mrmoro@o2.pl>

---
+++ SKraft/SKraft/Camera.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace SKraft
+{
+    public class Camera : Microsoft.Xna.Framework.GameComponent
+    {
+        public Matrix Projection { get; protected set; }
+        public Matrix View { get; protected set; }
+
+        private Vector2 mousePos;
+
+        public Camera(Game game, Vector3 position, Vector3 target, Vector3 up)
+            : base(game)
+        {
+            View = Matrix.CreateLookAt(position, target, up);
+            Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4,
+                                                             (float)Game.Window.ClientBounds.Width /
+                                                             (float)Game.Window.ClientBounds.Height, 1, 3000);
+
+            mousePos = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            KeyboardState state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.W))
+            {
+                View *= Matrix.CreateTranslation(0, 0, 0.1f);
+            }
+
+            if (state.IsKeyDown(Keys.S))
+            {
+                View *= Matrix.CreateTranslation(0, 0, -0.1f);
+            }
+
+            if (state.IsKeyDown(Keys.A))
+            {
+                View *= Matrix.CreateTranslation(0.1f, 0, 0);
+            }
+
+            if (state.IsKeyDown(Keys.D))
+            {
+                View *= Matrix.CreateTranslation(-0.1f, 0, 0);
+            }
+
+            View *= Matrix.CreateRotationY((Mouse.GetState().X - mousePos.X) / 75);
+            View *= Matrix.CreateRotationX((Mouse.GetState().Y - mousePos.Y) / 75);
+            mousePos.X = Mouse.GetState().X;
+            mousePos.Y = Mouse.GetState().Y;
+
+            base.Update(gameTime);
+        }
+    }
+}
---
+++ SKraft/SKraft/Cube.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace SKraft
+{
+    class Cube
+    {
+        private Model model;
+        private Matrix world = Matrix.Identity;
+
+        public void LoadContent(ContentManager content)
+        {
+            model = content.Load<Model>(@"models\cube");
+        }
+
+        Vector3 modelPosition = Vector3.Zero;
+        float modelRotation = 0.5f;
+
+        public void Draw(Camera camera)
+        {
+            // Copy any parent transforms.
+            Matrix[] transforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transforms);
+
+            // Draw the model. A model can have multiple meshes, so loop.
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                // This is where the mesh orientation is set, as well 
+                // as our camera and projection.
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.World = transforms[mesh.ParentBone.Index] *
+                        Matrix.CreateRotationY(modelRotation)
+                        * Matrix.CreateTranslation(modelPosition);
+                    effect.View = camera.View;
+                    effect.Projection = camera.Projection;
+                }
+                // Draw the mesh, using the effects set above.
+                mesh.Draw();
+            }
+        }
+    }
+}
